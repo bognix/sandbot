f5d4249e37eb537e39ca95bb57542258be598d35
nAndy <nandy@wikia-inc.com>
2017-08-09 16:28:07 +0200

    ADEN-5680 WIP use sqlite

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var RtmClient = require('@slack/client').RtmClient,
     token = process.env.SANDBOT_TOKEN || '',
-    sheet = require('./sheets'),
+    //sheet = require('./sheets'),
     Promise = require('bluebird'),
     request = require('request'),
     rtm = new RtmClient(token, {logLevel: 'error'}),
-    auth = sheet.authorize(),
+    //auth = sheet.authorize(),
+
+    sqlite3 = require('sqlite3').verbose(),
+    db = new sqlite3.Database(':memory:'),
 
     RTM_EVENTS = require('@slack/client').RTM_EVENTS,
     STATUS_PATTERN = /sandbot status/i,
@@ -12,58 +15,86 @@ var RtmClient = require('@slack/client').RtmClient,
     RELEASE_PATTERN = /(zwalniam|releasing) (sandbox|adeng)-/i,
     PING_PATTERN = /sandbot (zyjesz|ping)\?/i;
 
+db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS sandboxes(sandbox TEXT PRIMARY KEY ASC, team TEXT, owner TEXT);");
+
+    // Ad Engineering = G0GV00TC4
+    db.run("INSERT INTO sandboxes VALUES('sandbox-adeng01', 'G0GV00TC4', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-adeng02', 'G0GV00TC4', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-adeng03', 'G0GV00TC4', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-adeng04', 'G0GV00TC4', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-adeng05', 'G0GV00TC4', '');");
+    db.run("INSERT INTO sandboxes VALUES('adeng-fandom', 'G0GV00TC4', '');");
+
+    // X-Wing = C053B0DC2
+    db.run("INSERT INTO sandboxes VALUES('sanbox-dedicated', 'C053B0DC2', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-xw1', 'C053B0DC2', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-xw2', 'C053B0DC2', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-so', 'C053B0DC2', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-qa04', 'C053B0DC2', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-mercury', 'C053B0DC2', '');");
+    db.run("INSERT INTO sandboxes VALUES('sandbox-content', 'C053B0DC2', '');");
+
+    //db.each("SELECT sandbox, team, owner FROM sandboxes", function(err, row) {
+    //    console.log(row.team + "> " + row.sandbox + ": " + row.owner);
+    //});
+});
+
+db.close();
+
 rtm.start();
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-    if (message.text && STATUS_PATTERN.test(message.text)) {
 
-        getStatus(message.channel).then(function (data) {
-            var promises = [];
-
-            Object.keys(data.result).forEach(function (key) {
-                promises.push(parseSandboxStatus(key, data.result[key]));
-            });
-
-            Promise.all(promises).then(function (data) {
-                var parsedMsg = '```';
-                data.forEach(function (item) {
-                    parsedMsg += item[0] + ': ' + item[1] + '\n';
-                });
-                rtm.sendMessage(parsedMsg + '```', message.channel);
-            })
-        })
-    }
-
-    if (message.text && BOOK_PATTERN.test(message.text)) {
-        var sandboxName = getSandboxNameFromMessage(message),
-            msg = '<@' + message.user + '> ';
-
-        getPreviousUser(message.channel, sandboxName)
-            .then(function (previousUser) {
-                if (previousUser.result) {
-                    msg += ':-1: - <@' + previousUser.result + '> is using it';
-                    rtm.sendMessage(msg, message.channel)
-                } else {
-                    bookSandbox(message)
-                        .then(function () {
-                            msg += ':+1:';
-                            rtm.sendMessage(msg, message.channel)
-                        })
-                }
-            });
-    }
-
-    if (message.text && RELEASE_PATTERN.test(message.text)) {
-        releaseSandbox(message)
-            .then(function (data) {
-                if (data.response) {
-                    rtm.sendMessage(data.response, message.channel);
-                } else {
-                    var msg = '<@' + message.user + '> :+1:';
-                }
-                rtm.sendMessage(msg, message.channel)
-            })
-    }
+    //if (message.text && STATUS_PATTERN.test(message.text)) {
+    //
+    //    getStatus(message.channel).then(function (data) {
+    //        var promises = [];
+    //
+    //        Object.keys(data.result).forEach(function (key) {
+    //            promises.push(parseSandboxStatus(key, data.result[key]));
+    //        });
+    //
+    //        Promise.all(promises).then(function (data) {
+    //            var parsedMsg = '```';
+    //            data.forEach(function (item) {
+    //                parsedMsg += item[0] + ': ' + item[1] + '\n';
+    //            });
+    //            rtm.sendMessage(parsedMsg + '```', message.channel);
+    //        })
+    //    })
+    //}
+    //
+    //if (message.text && BOOK_PATTERN.test(message.text)) {
+    //    var sandboxName = getSandboxNameFromMessage(message),
+    //        msg = '<@' + message.user + '> ';
+    //
+    //    getPreviousUser(message.channel, sandboxName)
+    //        .then(function (previousUser) {
+    //            if (previousUser.result) {
+    //                msg += ':-1: - <@' + previousUser.result + '> is using it';
+    //                rtm.sendMessage(msg, message.channel)
+    //            } else {
+    //                bookSandbox(message)
+    //                    .then(function () {
+    //                        msg += ':+1:';
+    //                        rtm.sendMessage(msg, message.channel)
+    //                    })
+    //            }
+    //        });
+    //}
+    //
+    //if (message.text && RELEASE_PATTERN.test(message.text)) {
+    //    releaseSandbox(message)
+    //        .then(function (data) {
+    //            if (data.response) {
+    //                rtm.sendMessage(data.response, message.channel);
+    //            } else {
+    //                var msg = '<@' + message.user + '> :+1:';
+    //            }
+    //            rtm.sendMessage(msg, message.channel)
+    //        })
+    //}
 
     if (message.text && PING_PATTERN.test(message.text)) {
         rtm.sendMessage('zyje :+1:', message.channel);
