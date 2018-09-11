bdb211c7b6160ae6211418055588ee010821f732
hakubo <bukaj.kelo+github@gmail.com>
2018-09-11 14:45:50 +0200

    fix IWING_CHANNEL_ID in iwing sandboxes

--- db/006_iwing_sandboxes.js
+++ db/006_iwing_sandboxes.js
@@ -8,9 +8,9 @@ db.serialize(() => {
   db.run(`INSERT INTO sandboxes VALUES('sandbox-so', '${channels.IWING_CHANNEL_ID}', '');`);
   db.run(`INSERT INTO sandboxes VALUES('sandbox-mercury', '${channels.IWING_CHANNEL_ID}', '');`);
   db.run(`INSERT INTO sandboxes VALUES('sandbox-content', '${channels.IWING_CHANNEL_ID}', '');`);
-  db.run(`INSERT INTO sandboxes VALUES('sandbox-qa01', '${channels.IRIS_CHANNEL_ID}', '');`);
-  db.run(`INSERT INTO sandboxes VALUES('sandbox-qa02', '${channels.IRIS_CHANNEL_ID}', '');`);
-  db.run(`INSERT INTO sandboxes VALUES('sandbox-qa03', '${channels.IRIS_CHANNEL_ID}', '');`);
+  db.run(`INSERT INTO sandboxes VALUES('sandbox-qa01', '${channels.IWING_CHANNEL_ID}', '');`);
+  db.run(`INSERT INTO sandboxes VALUES('sandbox-qa02', '${channels.IWING_CHANNEL_ID}', '');`);
+  db.run(`INSERT INTO sandboxes VALUES('sandbox-qa03', '${channels.IWING_CHANNEL_ID}', '');`);
   db.run(`INSERT INTO sandboxes VALUES('sandbox-qa04', '${channels.IWING_CHANNEL_ID}', '');`);
 
   console.log('I-Wing sandboxes added.');
